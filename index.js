b2aa50d3a5398d2d0ac05db4dbdb15490b46f9f9
elecdeer <37701077+elecdeer@users.noreply.github.com>
2024-09-06 00:55:17 +0900

    fix: conform to linter rules

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-
-
-import noDescendantCombinator from "./lib/no_descendant_combinator.js";
 import classFormat from "./lib/class_format.js";
+import noDescendantCombinator from "./lib/no_descendant_combinator.js";
 
 const plugins = [noDescendantCombinator, classFormat];
 
--- lib/class_format.test.ts
+++ lib/class_format.test.ts
@@ -18,63 +18,63 @@ describe("basic class format", () => {
 		config: [true],
 
 		accept: [
-			{ code: `.good-component { }` },
-			{ code: `.good-component.-xyz { }` },
-			{ code: `.good-component.-no-xyz { }` },
-			{ code: `p { }` },
+			{ code: ".good-component { }" },
+			{ code: ".good-component.-xyz { }" },
+			{ code: ".good-component.-no-xyz { }" },
+			{ code: "p { }" },
 			{ code: `[aria-hidden="true"] { }` },
-			{ code: `._helper { }` },
-			{ code: `._helper._helper { }` },
-			{ code: `.my-component > .element { }` },
-			{ code: `.my-component > .element > .element2 { }` },
-			{ code: `.my-component > .ok { }` },
-			{ code: `.my-component + .my-component { }` },
-			{ code: `.my-component > a.-home { }` },
+			{ code: "._helper { }" },
+			{ code: "._helper._helper { }" },
+			{ code: ".my-component > .element { }" },
+			{ code: ".my-component > .element > .element2 { }" },
+			{ code: ".my-component > .ok { }" },
+			{ code: ".my-component + .my-component { }" },
+			{ code: ".my-component > a.-home { }" },
 		],
 		reject: [
 			{
-				code: `.badcomponent { }`,
+				code: ".badcomponent { }",
 				message: "Invalid component name: '.badcomponent' (rscss/class-format)",
 			},
 			{
-				code: `.badcomponent.-xyz { }`,
+				code: ".badcomponent.-xyz { }",
 				message:
 					"Invalid component name: '.badcomponent.-xyz' (rscss/class-format)",
 			},
 			{
-				code: `.badcomponent.-abc > .xyz { }`,
+				code: ".badcomponent.-abc > .xyz { }",
 				message:
 					"Invalid component name: '.badcomponent.-abc' (rscss/class-format)",
 			},
 			{
-				code: `.too-many.component-names { }`,
+				code: ".too-many.component-names { }",
 				message:
 					"Only one component name is allowed: '.too-many.component-names' (rscss/class-format)",
 			},
 			{
-				code: `._badhelper.-variant { }`,
+				code: "._badhelper.-variant { }",
 				message:
 					"Invalid helper name: '._badhelper.-variant' (rscss/class-format)",
 			},
 			{
-				code: `._badhelper.element { }`,
+				code: "._badhelper.element { }",
 				message:
 					"Invalid helper name: '._badhelper.element' (rscss/class-format)",
 			},
 			{
-				code: `.my-component > .bad_element { }`,
+				code: ".my-component > .bad_element { }",
 				message: "Invalid element name: '.bad_element' (rscss/class-format)",
 			},
 			{
-				code: `.my-component > .element.badvariant { }`,
+				code: ".my-component > .element.badvariant { }",
 				message: "Invalid variant name: '.badvariant' (rscss/class-format)",
 			},
 			{
-				code: `.my-component > .bad_nesting { }`,
+				code: ".my-component > .bad_nesting { }",
 				message: "Invalid element name: '.bad_nesting' (rscss/class-format)",
 			},
 			{
-				code: `.my-component > .-badvariant { }`,
+				code: ".my-component > .-badvariant { }",
 				message: "Variant has no element: '.-badvariant' (rscss/class-format)",
 			},
 		],
@@ -87,22 +87,22 @@ describe("pascal case", () => {
 		config: [true, { component: "pascal-case" }],
 
 		accept: [
-			{ code: `.GoodComponent { }` },
-			{ code: `.GoodComponent.-xyz { }` },
-			{ code: `.GoodComponent.-no-xyz { }` },
+			{ code: ".GoodComponent { }" },
+			{ code: ".GoodComponent.-xyz { }" },
+			{ code: ".GoodComponent.-no-xyz { }" },
 		],
 		reject: [
 			{
-				code: `.badComponent { }`,
+				code: ".badComponent { }",
 				message: "Invalid component name: '.badComponent' (rscss/class-format)",
 			},
 			{
-				code: `.badComponent.-xyz { }`,
+				code: ".badComponent.-xyz { }",
 				message:
 					"Invalid component name: '.badComponent.-xyz' (rscss/class-format)",
 			},
 			{
-				code: `.badComponent.-abc > .xyz { }`,
+				code: ".badComponent.-abc > .xyz { }",
 				message:
 					"Invalid component name: '.badComponent.-abc' (rscss/class-format)",
 			},
@@ -116,22 +116,22 @@ describe("custom format", () => {
 		config: [true, { component: "^([A-Z][a-z0-9]*)+$" }],
 
 		accept: [
-			{ code: `.GoodComponent { }` },
-			{ code: `.GoodComponent.-xyz { }` },
-			{ code: `.GoodComponent.-no-xyz { }` },
+			{ code: ".GoodComponent { }" },
+			{ code: ".GoodComponent.-xyz { }" },
+			{ code: ".GoodComponent.-no-xyz { }" },
 		],
 		reject: [
 			{
-				code: `.badComponent { }`,
+				code: ".badComponent { }",
 				message: "Invalid component name: '.badComponent' (rscss/class-format)",
 			},
 			{
-				code: `.badComponent.-xyz { }`,
+				code: ".badComponent.-xyz { }",
 				message:
 					"Invalid component name: '.badComponent.-xyz' (rscss/class-format)",
 			},
 			{
-				code: `.badComponent.-abc > .xyz { }`,
+				code: ".badComponent.-abc > .xyz { }",
 				message:
 					"Invalid component name: '.badComponent.-abc' (rscss/class-format)",
 			},
@@ -145,13 +145,13 @@ describe("depth", () => {
 		config: [true],
 
 		accept: [
-			{ code: `.my-component > .ok { }` },
-			{ code: `.my-component > .ok > .ok { }` },
-			{ code: `.my-component > .ok > .ok > .ok { }` },
+			{ code: ".my-component > .ok { }" },
+			{ code: ".my-component > .ok > .ok { }" },
+			{ code: ".my-component > .ok > .ok > .ok { }" },
 		],
 		reject: [
 			{
-				code: `.my-component > .ok > .ok > .ok > .bad { }`,
+				code: ".my-component > .ok > .ok > .ok > .bad { }",
 				message:
 					"Component too deep: '.my-component > .ok > .ok > .ok > .bad' (rscss/class-format)",
 			},
@@ -164,10 +164,10 @@ describe("whitelist", () => {
 		ruleName: "rscss/class-format",
 		config: [true, { componentWhitelist: ["btn"] }],
 
-		accept: [{ code: `.btn > .lol { }` }, { code: `.btn.-variant { }` }],
+		accept: [{ code: ".btn > .lol { }" }, { code: ".btn.-variant { }" }],
 		reject: [
 			{
-				code: `.btn.bad { }`,
+				code: ".btn.bad { }",
 				message: "Invalid variant name: '.bad' (rscss/class-format)",
 			},
 		],
